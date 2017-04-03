fdd819c4a0d8f1126832583a9aa8184aa7b7e017
edwinchen85 <edwinchen85@gmail.com>
2017-04-03 22:03:30 +0800

    When progress bar is more than 70%, add special class target for alternate styling.

--- js/custom.js
+++ js/custom.js
@@ -12,6 +12,11 @@ $(document).ready(function() {
 
   // Create the progress bar
   createProgressBar(progress, function() {
+  // if the progress value is higher than 70, we apply a separate class to the progress bar
+  if( progress > 70 ) {
+    $('.ui-progressbar-value').addClass('inwards');
+  }
+
   // Append the description to the inner progress bar layer
   $('<div class="progress-description">'+ '<p><strong>'+ progress + '%</strong></p><p>'+ progressDescription +'</p></div>').appendTo(".ui-progressbar-value");
   });
